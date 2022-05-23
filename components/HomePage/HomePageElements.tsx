61312fcfd0571c593650931e2f7d87237c832ccf
Brian Hough <brianhoughmedia@gmail.com>
2022-05-22 20:37:45 -0400

    Connect Wallet + Layout + NavBar + Footer

---
+++ components/HomePage/HomePageElements.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+export const RandomizingHeader = styled.div`
+  margin-top: 30px;
+  text-align: center;
+  font-size: 30pt;
+`;
+
+export const SubHeader = styled.div`
+  margin-top: 30px;
+  text-align: center;
+  font-size: 15pt;
+`;
+
+export const PeopleBlobs = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-top: 40px;
+`;
